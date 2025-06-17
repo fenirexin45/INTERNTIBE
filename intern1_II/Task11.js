772ab61fe9a54a416512b27d93e66daf7f6dade4
fenirexin45 <feninarex.j@gmail.com>
2025-06-17 14:56:01 +0530

    Delete intern1_II directory

--- intern1_II/Task11.js
+++
@@ -1,7 +0,0 @@
-function square_of(x) {
-    return x * x;
-}
-
-console.log("\t\tSquare of given number!!");
-
-console.log("\nSquare of 11:", square_of(11));
--- intern1_II/Task20iii.js
+++
@@ -1,6 +0,0 @@
-
-console.log("\t\tSAME ELEMENTS");
-
-var list = [10, 20, 30, 40, 10];
-var result = (list[0] === list[list.length - 1]);
-console.log("\nFirst and last are same:", result);
